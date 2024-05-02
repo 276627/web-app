f473f37dd060e0ff528ecda2d29bc6157ca8e5ab
276627 <157831352+276627@users.noreply.github.com>
2024-05-02 11:11:09 -0500

    May2

---
+++ scripts.js
@@ -0,0 +1,21 @@
+function play(playerChoice) {
+    var choices = ["rock", "paper", "scissors"];
+    var computerChoice = choices[Math.floor(Math.random() * 3)];
+  
+    var result = "";
+  
+    if (playerChoice === computerChoice) {
+      result = "It's a tie!";
+    } else if (
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "paper" && computerChoice === "rock") ||
+      (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+      result = "You win!";
+    } else {
+      result = "You lose!";
+    }
+  
+    document.getElementById("result").innerText = `You chose ${playerChoice}, computer chose ${computerChoice}. ${result}`;
+  }
+ 
